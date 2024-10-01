84e0adc086a7e1f885183f9c790c0a5eb59347d3
klaviwka <klaviwka@mail.ru>
2024-10-01 09:41:37 +0300

    Add files via upload

---
+++ module_9_6.py
@@ -0,0 +1,9 @@
+def all_variants(text: str):
+    length = len(text)
+    for size in range(1, length + 1):
+        for i in range(length - size + 1):
+            yield text[i:i + size]
+
+a = all_variants("abc")
+for n in a:
+    print(n)
